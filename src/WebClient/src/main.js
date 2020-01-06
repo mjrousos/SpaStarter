bbf9087499fc6fffecebd5b8ada9a6a4bfcc3a31
Mike Rousos <Mike@MJRousos.com>
2020-01-06 18:09:15 -0500

    Add bootstrap to component starter

--- src/WebClient/src/main.js
+++ src/WebClient/src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import "bootstrap";
+import "./styles/style.scss";
 
 Vue.config.productionTip = false;
 
