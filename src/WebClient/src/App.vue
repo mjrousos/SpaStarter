c5609005276384ed654ffb42e6b57b1b695eb8a6
Mike Rousos <Mike@MJRousos.com>
2020-01-07 10:49:37 -0500

    Add footer component

--- src/WebClient/src/App.vue
+++ src/WebClient/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <NavBar />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "app",
   components: {
-    NavBar
+    NavBar,
+    Footer
   }
 };
 </script>
---
+++ src/WebClient/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="nav fixed-bottom navbar-dark bg-dark px-1">
+    <span class="navbar-text navbar-dark">
+      &copy; {{ currentYear }} M.J. Rousos
+    </span>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data: function() {
+    return {
+      currentYear: new Date().getFullYear()
+    };
+  }
+};
+</script>
